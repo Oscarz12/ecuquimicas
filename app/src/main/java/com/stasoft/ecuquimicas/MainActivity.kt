96481a712bcfae12200fc06d317327e7731d46be
Oscar Zuñiga G <ydrojasri@iemsantateresita.edu.co>
2022-08-25 12:47:26 -0500

    initial commit

---
+++ app/src/main/java/com/stasoft/ecuquimicas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stasoft.ecuquimicas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+// este es un comentario
+}
